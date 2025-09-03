7dc1b59e8a699dc28b150926495abc93b2b95625
SimoSabev <sabevsimeon08@gmail.com>
2025-09-03 18:22:04 +0300

    style: fixed bottom image's text appear on mobile on hero page

--- components/materials-section.tsx
+++ components/materials-section.tsx
@@ -76,10 +76,10 @@ export function MaterialsSection() {
                 <div className="absolute inset-0 bg-black/30" /> {/* darker overlay */}
             </div>
 
-            <div className="absolute top-20 right-8 z-10 max-w-md hidden lg:block">
+            <div className="absolute top-20 right-8 z-10 px-8 lg:px-0 min-w-full lg:min-w-md lg:max-w-md left-1/2 transform -translate-x-1/2 lg:translate-x-0 lg:left-auto lg:top-20 lg:right-8">
                 <Reveal delay={0.3}>
-                    <blockquote className="pl-0 py-4">
-                        <p className="text-xl text-white leading-relaxed italic lg:text-base font-medium">
+                    <blockquote className="w-full pl-0 py-4 text-center lg:text-left">
+                        <p className="w-full text-base text-white leading-relaxed italic font-medium">
                             Whether it&apos;s sourcing the finest materials, selecting the perfect furniture
                             and accessories, or managing complex refits, we approach each project with
                             care, attention to detail, and a commitment to service excellence.
